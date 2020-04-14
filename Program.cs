14a0826f0ae7222fb5a6bb3271ca9427f9d158b1
Danilo Nascimento Cunha <id4ni10@gmail.com>
2020-04-14 11:17:15 -0300

    add travis

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ssl_renewal_watcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
